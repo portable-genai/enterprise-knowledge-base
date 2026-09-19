portable-genai/disputes-chargebacks-manager

Make customer-managed encryption optional, and off by default
Every binding to the stack's Cloud KMS key now sits behind cmek_enabled,
default false: the key ring, the key, every grant to a service agent, and
every store, log bucket, registry and revision that took the key. Block
bindings became dynamic blocks; scalar bindings read the key through one(),
which is null when the key does not exist and omits the attribute rather
than pointing it at nothing. The Terraform tests that assert on key material
turn the gate on to keep meaning what they meant.

Why the default is off: a key ring can never be deleted, a log bucket that
has CMEK can never drop it, and registries and document stores take their
key at creation. None of that changes an answer or a screen, and everything
is encrypted at rest with Google-managed keys regardless. A deployment with
a customer whose data it must shred, whose key access must be audited or
whose keys must live in an HSM sets it true in its own tfvars BEFORE the
first apply. Where a stack already applied it, the key's prevent_destroy
refuses a plan that omits the line, which is the right answer: the stores it
bound stay bound.

The vendored deploy-and-residency-hardening skill says the same.

infra/terraform/kms.tf
@@ -16,6 +16,7 @@
 # NOTE for template maintainers: copied into a render VERBATIM. No Jinja here.
 
 resource "google_kms_key_ring" "cmek" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = local.kms_ring_name
   location = local.region # regional, in-country key material (P-03)
 
@@ -23,8 +24,9 @@ resource "google_kms_key_ring" "cmek" {
 }
 
 resource "google_kms_crypto_key" "cmek" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = local.kms_key_name
-  key_ring = google_kms_key_ring.cmek.id
+  key_ring = one(google_kms_key_ring.cmek[*].id)
 
   purpose         = "ENCRYPT_DECRYPT"
   rotation_period = "7776000s" # 90 days
@@ -58,14 +60,16 @@ data "google_project" "this" {
 
 # Cloud Logging, for the locked WORM audit bucket (logging_worm.tf).
 resource "google_kms_crypto_key_iam_member" "logging" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-logging.iam.gserviceaccount.com"
 }
 
 # Cloud Run, for the serving revision's own encrypted storage (production_edge.tf).
 resource "google_kms_crypto_key_iam_member" "run" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@serverless-robot-prod.iam.gserviceaccount.com"
 }
@@ -73,7 +77,8 @@ resource "google_kms_crypto_key_iam_member" "run" {
 # Vertex AI, for whatever narration or classification surface this vertical binds. The binding
 # is in the baseline so the key is already bound on the day the adapter arrives.
 resource "google_kms_crypto_key_iam_member" "aiplatform" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
 }
@@ -82,7 +87,8 @@ resource "google_kms_crypto_key_iam_member" "aiplatform" {
 # bucket is per-repo, the key binding is not the interesting part of that commit, and a bucket
 # created without it is silently encrypted under Google-managed keys.
 resource "google_kms_crypto_key_iam_member" "storage" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gs-project-accounts.iam.gserviceaccount.com"
 }

infra/terraform/logging_worm.tf
@@ -36,8 +36,11 @@ resource "google_logging_project_bucket_config" "worm_audit" {
   locked = var.worm_locked
 
   # CMEK on the log bucket (P-09): explicit, because it does not cascade.
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.cmek.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.cmek[*].id)
+    }
   }
 
   depends_on = [

infra/terraform/outputs.tf
@@ -26,7 +26,7 @@ output "residency_allowlist" {
 # --------------------------------- KMS -------------------------------------- #
 output "kms_key" {
   description = "Regional CMEK crypto key id, bound to Cloud Logging, Cloud Run, Vertex AI and Cloud Storage service agents."
-  value       = google_kms_crypto_key.cmek.id
+  value       = one(google_kms_crypto_key.cmek[*].id)
 }
 
 # ------------------------------- WORM logging ------------------------------- #

portable-genai/enterprise-knowledge-base

infra/terraform/kms.tf
@@ -10,6 +10,7 @@
 #         global/multi-region key. Regional CMEK is what pins crypto material in-country.
 
 resource "google_kms_key_ring" "kb" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = "enterprise-knowledge-base-ring"
   location = var.region # asia-southeast1 : regional, in-country key material (P-03)
 
@@ -17,8 +18,9 @@ resource "google_kms_key_ring" "kb" {
 }
 
 resource "google_kms_crypto_key" "kb" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = "enterprise-knowledge-base-cmek"
-  key_ring = google_kms_key_ring.kb.id
+  key_ring = one(google_kms_key_ring.kb[*].id)
 
   purpose         = "ENCRYPT_DECRYPT"
   rotation_period = "7776000s" # 90 days : periodic rotation for key hygiene
@@ -108,28 +110,32 @@ resource "terraform_data" "logging_cmek_identity" {
 }
 
 resource "google_kms_crypto_key_iam_member" "artifact_registry" {
-  crypto_key_id = google_kms_crypto_key.kb.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.kb[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_project_service_identity.artifact_registry.email}"
 }
 
 # AlloyDB service agent.
 resource "google_kms_crypto_key_iam_member" "alloydb" {
-  crypto_key_id = google_kms_crypto_key.kb.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.kb[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_project_service_identity.alloydb.email}"
 }
 
 # Cloud Storage service agent (CMEK on the corpus bucket).
 resource "google_kms_crypto_key_iam_member" "storage" {
-  crypto_key_id = google_kms_crypto_key.kb.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.kb[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${data.google_storage_project_service_account.storage.email_address}"
 }
 
 # Cloud Logging service agent (CMEK on the WORM bucket).
 resource "google_kms_crypto_key_iam_member" "logging" {
-  crypto_key_id = google_kms_crypto_key.kb.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.kb[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${data.google_logging_project_cmek_settings.logging.service_account_id}"
 
@@ -138,14 +144,16 @@ resource "google_kms_crypto_key_iam_member" "logging" {
 
 # Cloud Run service agent encrypts API revisions with the regional key.
 resource "google_kms_crypto_key_iam_member" "cloud_run" {
-  crypto_key_id = google_kms_crypto_key.kb.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.kb[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_project_service_identity.cloud_run.email}"
 }
 
 # The system-created _Trace bucket uses the Observability service identity, not the Trace writer.
 resource "google_kms_crypto_key_iam_member" "observability" {
-  crypto_key_id = google_kms_crypto_key.kb.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.kb[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_project_service_identity.observability.email}"
 }

infra/terraform/logging_worm.tf
@@ -31,8 +31,11 @@ resource "google_logging_project_bucket_config" "default" {
   location  = var.region
   bucket_id = "_Default"
 
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.kb.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.kb[*].id)
+    }
   }
 
   depends_on = [google_kms_crypto_key_iam_member.logging]
@@ -43,8 +46,11 @@ resource "google_logging_project_bucket_config" "required" {
   location  = var.region
   bucket_id = "_Required"
 
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.kb.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.kb[*].id)
+    }
   }
 
   depends_on = [google_kms_crypto_key_iam_member.logging]
@@ -77,8 +83,11 @@ resource "google_logging_project_bucket_config" "worm_audit" {
   }
 
   # CMEK on the log bucket (P-09) : explicit, does not cascade.
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.kb.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.kb[*].id)
+    }
   }
 
   depends_on = [

infra/terraform/outputs.tf
@@ -46,7 +46,7 @@ output "artifact_registry_repository" {
 # --------------------------------- KMS -------------------------------------- #
 output "kms_key" {
   description = "Regional CMEK crypto key id (settings.yaml kms_key / KB_KMS_KEY)."
-  value       = google_kms_crypto_key.kb.id
+  value       = one(google_kms_crypto_key.kb[*].id)
 }
 
 # --------------------------------- AlloyDB ---------------------------------- #